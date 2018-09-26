9c58599e2ca95fca6718a3f5cb43fb1fbf93a6bc
Maiky Ramon <298064@unoesc.lan>
2018-09-25 21:30:13 -0300

    commit inicial

---
+++ app/src/main/java/pdm/unoesc/edu/br/fashionweather/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package pdm.unoesc.edu.br.fashionweather.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pdm.unoesc.edu.br.fashionweather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
